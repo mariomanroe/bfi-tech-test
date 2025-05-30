b2db92028e8e34017d474db092aa87e966800918
Mario Desendi <105722129+mariomanroe@users.noreply.github.com>
2025-05-30 16:36:14 +0700

    Update curl_test.sh

--- curl_test.sh
+++ curl_test.sh
@@ -1,3 +1,3 @@
-curl -X POST http://localhost:8000/predict \
+curl -X POST http://http://ip172-18-0-23-d0sndkol2o9000dn1eng-8000.direct.labs.play-with-docker.com/predict \
 -H "Content-Type: application/json" \
 -d '{"fixed_acidity":7.4,"volatile_acidity":0.7,"citric_acid":0,"residual_sugar":1.9,"chlorides":0.076,"free_sulfur_dioxide":11.0,"total_sulfur_dioxide":34.0,"density":0.9978,"pH":3.51,"sulphates":0.56,"alcohol":9.4}'
